fb1b2368621719e635fd9d77cc9f6ad5d2a3243c
eden <eelmalich2@gmail.com>
2019-10-08 10:00:03 +0300

    inital commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,72 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <imageBrowser />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+
+<script lang="ts">
+import Vue from "vue";
+import imageBrowser from "@/components/imageBrowser.vue";
+import Component from "vue-class-component";
+@Component({
+  components: {
+    imageBrowser
   }
+})
+export default class App extends Vue {}
+</script>
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Calibri, Candara, Segoe, Segoe UI, Optima, Arial, sans-serif;
+}
+header {
+  background: #333;
+  height: 60px;
+}
+.header-text {
+  text-align: center;
+  font-size: 42px;
+  color: white;
+  font-weight: 600;
+}
+.Search-att {
+  padding: 1.5rem;
+  border-bottom: 1px solid #e7e7e7;
+  display: flex;
+  justify-content: center;
+}
+.Search-att input[type="text"] {
+  width: 15%;
+  height: 25px;
+  text-indent: 15px;
+}
+.Search-att input[type="submit"] {
+  height: 25px;
+  background: #333;
+  color: white;
+}
+.image-box {
+  border-radius: 5px 5px 0 0;
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+  padding: 5px;
+}
+.Container-images {
+  padding-right: 75px;
+  padding-left: 75px;
+  height: 600px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  overflow-y: scroll;
 }
 </style>
